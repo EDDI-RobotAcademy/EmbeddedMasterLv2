326e3667370913f27168d1fd7b98eac2f67920b3
Gone <60423816+0xCodExpert@users.noreply.github.com>
2022-07-02 00:02:38 +0900

    리눅스 프로그래밍 과제

--- PSH/Linux_system_programming/hw/file_change_win.c
+++ PSH/Linux_system_programming/hw/file_change_win.c
@@ -10,7 +10,7 @@ int write_to_file(void)
         return -1;
     }
     printf("after fopen(). offset=%ld\n", ftell(fp));
-    fputs("I am gone I am gone", fp);
+    fputs("I am gone", fp);
     printf("before fclose(). offset=%ld\n", ftell(fp));
     printf("---------------------------------\n");
     fclose(fp);
@@ -20,11 +20,13 @@ int read_from_file(void)
 {
     FILE *fp;
     char buf[1024];
+    char write_buf[1024];
     char tmp_buf[1024];
     char *tok;
     int argc = 0;
     int cmp = 0;
-    char *argv[1024];
+    // char *argv[1024];
+    char argv[1024][1024];
     char *delim = " ";
 
     fp = fopen("data", "r+");
@@ -42,23 +44,32 @@ int read_from_file(void)
 
     for (tok = strtok(buf, " "); tok != NULL; tok = strtok(NULL, " "))
     {
-        printf("%s\n",tok);
-        argv[argc++] = (char *) tok;
+        // printf("%s\n",tok);
+        strcpy(argv[argc++], tok);
+        // argv[argc++] = (char *) tok;
+        // argv[argc++] = *tok;
     }
 
+    printf("%s\n", argv[0]);
+    printf("%s\n", argv[1]);
+    printf("%s\n", argv[2]);
+
     for (cmp = 0; cmp < argc; cmp++){
-        if(strcmp (argv[cmp], "am") == 0) {
-            strcpy(argv[cmp], "or");
+        if(strcmp(argv[cmp], "am") == 0) {
+            strcpy(argv[cmp], "are");
             // printf("%s\n",argv[cmp]);
         }
     }
 
-    // printf("%s\n", argv[0]);
-    // printf("%s\n", argv[1]);
-    // printf("%s\n", argv[2]);
-    // printf("%s\n", argv[3]);
-    // printf("%s\n", argv[4]);
-    // printf("%s\n", argv[5]);
+    printf("%s\n", argv[0]);
+    printf("%s\n", argv[1]);
+    printf("%s\n", argv[2]);
+
+    sprintf(write_buf, "%s %s %s", argv[0], argv[1], argv[2]);
+
+    fseek(fp,0,SEEK_SET);
+    fputs(write_buf, fp);
+
     // printf("argv[%d] = %s",argc, argv[argc]);
     // fseek(fp,0,SEEK_SET);
     // printf("after fseek(). offset=%ld\n", ftell(fp));
