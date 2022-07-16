ae6829dca045c296401ba9242faac3a91e7a3586
silenc3502 <gcccompil3r@gmail.com>
2022-07-16 10:40:14 +0900

    first ans

---
+++ LSH/lsp/first/adv1.c
@@ -0,0 +1,68 @@
+#include <unistd.h>
+#include <sys/types.h>
+#include <fcntl.h>
+
+#include <stdio.h>
+#include <string.h>
+
+#define SLABSIZE		64
+
+int src_start_loc[SLABSIZE] = { 0 };
+int src_loc_cnt = 0;
+
+int dst_start_loc[SLABSIZE] = { 0 };
+int dst_loc_cnt = 0;
+
+char *apple = "apple";
+char *watermelon = "go";
+
+int srclen;
+int dstlen;
+
+int main(void)
+{
+	int i;
+	int cnt = 0;
+	int read_byte;
+	int addsize = 0;
+
+	int difflen = 0;
+	int total = 0;
+
+	char buf[SLABSIZE] = { 0 };
+	char tmp_buf[SLABSIZE] = { 0 };
+	char preset[SLABSIZE] = "apple test apple test apple test\n";
+
+	srclen = strlen(apple);
+	dstlen = strlen(watermelon);
+	difflen = dstlen - srclen;
+	total = strlen(preset);
+
+	int rwfd = open("test.txt", O_RDWR | O_TRUNC | O_CREAT, 0644);
+
+	write(rwfd, preset, strlen(preset));
+	lseek(rwfd, 0, SEEK_SET);
+
+	for (i = 0; i < strlen(preset) + addsize; i++)
+	{
+		lseek(rwfd, i, SEEK_SET);
+
+		if (read(rwfd, buf, srclen) == srclen && !strncmp(buf, apple, srclen))
+		{
+			cnt++;
+			read_byte = read(rwfd, tmp_buf, SLABSIZE);
+			lseek(rwfd, i, SEEK_SET);
+			write(rwfd, watermelon, dstlen);
+			write(rwfd, tmp_buf, read_byte);
+			addsize = (dstlen - srclen) * cnt;
+			i += dstlen;	// 좀 더 빠른 순회
+		}
+	}
+
+	if (difflen < 0)
+	{
+		ftruncate(rwfd, total + difflen * cnt);
+	}
+
+	return 0;
+}
